5abe6fad8f484ea5dc3fc2db060537ab36086cdb
Neha Nooka <52026271+NehaNooka@users.noreply.github.com>
2025-01-14 12:37:31 -0500

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "NEHA NOOKA",
+  profile: '/profile.jpeg',
+  designation: "Software Developer",
+  description: "My name is NEHA NOOKA, and I am a passionate and dedicated programmer with a strong drive for learning and exploring new technologies. I am a quick learner with a self-motivated approach, always eager to solve challenging problems and create impactful solutions.",
+  description2: "My expertise spans multiple areas of web and software development, with a strong foundation in JavaScript and a keen interest in building dynamic, user-friendly applications. I thrive on making technology accessible and effective, contributing to innovative projects that leave a mark. I am open to exciting job opportunities that align with my skills and passion for development.",
+  email: 'nookaneha@gmail.com',
+  phone: '+16673358435',
+  address: 'Seattle WA, USA (Open to relocation)',
+  github: 'https://github.com/NehaNooka',
+  facebook: 'https://www.facebook.com/NehaNooka/',
+  linkedIn: 'https://www.linkedin.com/in/neha-nooka/',
+  twitter: 'https://twitter.com/nehanooka',
+  stackOverflow: 'https://stackoverflow.com/users/29148012/neha-nooka',
+  leetcode: "https://leetcode.com/u/nehanooka/",
+  devUsername: "nehanooka",
+  resume: "https://drive.google.com/file/d/1nzo6AEhlryO4dxACzpd0Hw8Krm0fuA_C/view?usp=sharing"
+}
